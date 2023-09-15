ca6fcbc258b70611fea36bacfaaa152d46d94a5e
fardantif21 <fardan.al.tif421@polban.ac.id>
2023-09-16 05:39:13 +0700

    update fitur create

--- Special Mission/go-crud/controllers/pasiencontrollers/pasiencontroller.go
+++ Special Mission/go-crud/controllers/pasiencontrollers/pasiencontroller.go
@@ -1,7 +1,6 @@
 package pasiencontroller
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
 
--- Special Mission/go-crud/models/pasienmodel.go
+++ Special Mission/go-crud/models/pasienmodel.go
@@ -25,7 +25,7 @@ func NewPasienModel() *PasienModel {
 
 func (p *PasienModel) Create(pasien entities.Pasien) bool {
 
-	result, err := p.conn.Exec("insert into pasien (nama_lengkap, nik, jenis_kelamin, tempat_lahir, tanggal_lahir, alamat, no_hp) value(?, ?, ?, ?, ?, ?, ?)", pasien.NamaLengkap, pasien.NIK, pasien.JenisKelamin, pasien.TempatLahir, pasien.TanggalLahir, pasien.Alamat, pasien.NoHp)
+	result, err := p.conn.Exec("insert into pasien (nama_lengkap, nik, jenis_kelamin, tempat_lahir, tanggal_lahir, alamat, no_hp) values(?, ?, ?, ?, ?, ?, ?)", pasien.NamaLengkap, pasien.NIK, pasien.JenisKelamin, pasien.TempatLahir, pasien.TanggalLahir, pasien.Alamat, pasien.NoHp)
 
 	if err != nil {
 		fmt.Println(err)
