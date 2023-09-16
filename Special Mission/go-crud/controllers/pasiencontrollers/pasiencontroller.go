d0a3670f0d624993ac3ab1760ba51ba8d4615dee
fardantif21 <fardan.al.tif421@polban.ac.id>
2023-09-16 07:16:26 +0700

    add fitur update and delete

--- Special Mission/go-crud/controllers/pasiencontrollers/pasiencontroller.go
+++ Special Mission/go-crud/controllers/pasiencontrollers/pasiencontroller.go
@@ -1,6 +1,7 @@
 package pasiencontroller
 
 import (
+	"strconv"
 	"html/template"
 	"net/http"
 
@@ -59,9 +60,57 @@ func Add(response http.ResponseWriter, request *http.Request) {
 }
 
 func Edit(response http.ResponseWriter, request *http.Request) {
-	
+
+	if request.Method == http.MethodGet {
+
+		queryString := request.URL.Query()
+		id, _ := strconv.ParseInt(queryString.Get("id"), 10, 64)
+
+		var pasien entities.Pasien
+		pasienModel.Find(id, &pasien)
+
+		data := map[string]interface{} {
+			"pasien": pasien,
+		}
+
+		temp, err := template.ParseFiles("views/pasien/edit.html")
+		if err != nil {
+			panic(err)
+		}
+		temp.Execute(response, data)
+
+	} else if request.Method == http.MethodPost {
+
+		request.ParseForm()
+
+		var pasien entities.Pasien
+		pasien.Id, _ = strconv.ParseInt(request.Form.Get("id"), 10, 64)
+		pasien.NamaLengkap = request.Form.Get("nama_lengkap")
+		pasien.NIK = request.Form.Get("nik")
+		pasien.JenisKelamin = request.Form.Get("jenis_kelamin")
+		pasien.TempatLahir = request.Form.Get("tempat_lahir")
+		pasien.TanggalLahir = request.Form.Get("tanggal_lahir")
+		pasien.Alamat = request.Form.Get("alamat")
+		pasien.NoHp = request.Form.Get("no_hp")
+
+		pasienModel.Update(pasien)
+		data := map[string]interface{} {
+			"pesan": "Data berhasil diperbarui",
+		}
+
+		temp, _ := template.ParseFiles("views/pasien/edit.html")
+		temp.Execute(response, data)
+	}
+
 }
 
 func Delete(response http.ResponseWriter, request *http.Request) {
 	
+	queryString := request.URL.Query()
+	id, _ := strconv.ParseInt(queryString.Get("id"), 10, 64)
+
+	pasienModel.Delete(id)
+
+	http.Redirect(response, request, "/pasien", http.StatusSeeOther)
+
 }
